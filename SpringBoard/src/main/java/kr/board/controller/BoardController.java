50f8c465a95e977fa61641be253d3a52c2fd9315
jarvis <jarvis@DESKTOP-DMC9ABN>
2023-02-02 20:06:08 +0900

    Spring board v1.1

--- SpringBoard/src/main/java/kr/board/controller/BoardController.java
+++ SpringBoard/src/main/java/kr/board/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.board.entity.Board;
 import kr.board.mapper.BoardMapper;
@@ -21,13 +22,17 @@ public class BoardController {
 	private BoardMapper mapper;
 	// HandlerMapping
 
+	@RequestMapping("/")
+	public String main() {
+		return "main";
+	}
+	
+	//@ResponseBody -> jackson-dtabind(객체를 -> JSON 데이터 포멧으로 변환)  
+	
 	@RequestMapping("/boardList.do")
-	public String boardList(Model model) {
-		
+	public @ResponseBody List<Board> boardList(){	//@ResponseBody: JSON 사용시 사용
 		List<Board> list = mapper.getLists();
-		model.addAttribute("list", list);
-		return "boardList"; // /WEB-INF/views/boardList.jsp -> forward --View
-							// Resolver(servlet-contenxt.xml)설정이 되어있음
+		return list;	//JSON 데이터 형식으로 변환(API)해서 리턴(응답)하겠다.
 	}
 	@GetMapping("/boardForm.do")
 	public String boardForm() {
