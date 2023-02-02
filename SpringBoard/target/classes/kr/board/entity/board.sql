d0e3e3510267a98cacd2c6131ecacc24ef578fa4
jarvis <jarvis@DESKTOP-DMC9ABN>
2023-02-02 19:57:10 +0900

    Spring board v1.0

---
+++ SpringBoard/target/classes/kr/board/entity/board.sql
@@ -0,0 +1,22 @@
+create table board(
+idx number not null,
+title varchar2(100) not null,
+content varchar2(2000) not null,
+writer varchar2(30) not null,
+indate date default sysdate,
+count number default 0,
+primary key(idx)
+);
+create sequence board_idx;
+
+--시퀸스 조회 및 삭제
+SELECT * FROM USER_SEQUENCES;
+DROP SEQUENCE board_idx;
+
+insert into board(idx, title, content, writer)
+values(board_idx.nextval, '스프링게시판','스프링게시판','관리자');
+insert into board(idx, title, content, writer)
+values(board_idx.nextval, '스프링게시판','스프링게시판','배민재');
+insert into board(idx, title, content, writer)
+values(board_idx.nextval, '스프링게시판','스프링게시판','손님');
+select * from board order by idx desc
